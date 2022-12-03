81a2731a82e13bef02713f19e17a3753b755cbc9
Bob Oyier <boboyier92@gmail.com>
2022-12-03 15:30:44 +0300

    "Failing two tests in this lab but I believe my solutions are correct. Will consult the TM later."

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const users = ["Bobby", "salim", "Mikey", "Peter", "Bobby", "Salim"];
+
+function findMatching(array, string) {
+  let newArray = array.filter((input) =>
+    input[0].toUpperCase().match(string[0].toUpperCase())
+  );
+  return newArray;
+}
+
+console.log(findMatching(users, "salim"));
+
+function fuzzyMatch(array, string) {
+  let newArray = array.filter((input) => input.startsWith(string[0]));
+  return newArray;
+}
+
+function matchName(array, string) {
+  let newArray = array.filter(input => input.name === string)
+  return newArray
+}
